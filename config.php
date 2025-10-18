156f1160b2bba288bb14594a0e8ff88a19e14d08
Douglas Augusto <149178392+Doug1518@users.noreply.github.com>
2025-10-18 10:53:16 -0300

    Update config.php
    removendo db password

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // Credenciais do Banco de Dados
 define('DB_SERVER', 'localhost'); // Geralmente 'localhost'
 define('DB_USERNAME', 'root'); // Substitua pelo seu usuário
-define('DB_PASSWORD', '1234'); // Substitua pela sua senha
+define('DB_PASSWORD', ''); // Substitua pela sua senha
 define('DB_NAME', 'sistema_escolar'); // Nome do banco de dados conforme seu script
 
 // URLs Base (ajuste conforme a sua estrutura de projeto)
